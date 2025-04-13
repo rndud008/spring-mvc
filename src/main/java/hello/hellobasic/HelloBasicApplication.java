deb3af0c647668dcb6e08c3aa5755ddeb8e83d18
rndud008 <rndud008@gmail.com>
2025-04-13 20:34:10 +0900

    commit

---
+++ src/main/java/hello/hellobasic/HelloBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.hellobasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/hellobasic/HelloBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellobasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
