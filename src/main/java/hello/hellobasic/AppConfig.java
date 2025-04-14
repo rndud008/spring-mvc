667dfbe27b51ed628a625124dc907bcab65105b4
rndud008 <rndud008@gmail.com>
2025-04-14 22:47:31 +0900

    [스프링 핵심 원리 이해2] - AppConfig 리팩터링

--- src/main/java/hello/hellobasic/AppConfig.java
+++ src/main/java/hello/hellobasic/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.hellobasic;
 
+import hello.hellobasic.dicount.DiscountPolicy;
 import hello.hellobasic.dicount.FixDiscountPolicy;
+import hello.hellobasic.member.MemberRepository;
 import hello.hellobasic.member.MemberService;
 import hello.hellobasic.member.MemberServiceImpl;
 import hello.hellobasic.member.MemoryMemberRepository;
@@ -9,11 +11,19 @@ import hello.hellobasic.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 
 }
