e703f972603052537fb930d2e85daa20a0d608ee
rndud008 <rndud008@gmail.com>
2025-04-14 23:11:45 +0900

    [스프링 핵심 원리 이해2] - 스프링으로 전환

--- src/main/java/hello/hellobasic/AppConfig.java
+++ src/main/java/hello/hellobasic/AppConfig.java
@@ -9,20 +9,24 @@ import hello.hellobasic.member.MemberServiceImpl;
 import hello.hellobasic.member.MemoryMemberRepository;
 import hello.hellobasic.order.OrderService;
 import hello.hellobasic.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
-
-    private static MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
-
+    @Bean
+    public  MemberRepository memberRepository() {
+            return new MemoryMemberRepository();
+        }
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy(){
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
