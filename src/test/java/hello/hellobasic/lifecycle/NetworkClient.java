ea5f2c9da87a0537d80d87c7067f4658987b1c7d
rndud008 <rndud008@gmail.com>
2025-04-17 01:37:23 +0900

    [빈 생명주기 콜백] - 인터페이스 InitializingBean, DisposableBean

--- src/test/java/hello/hellobasic/lifecycle/NetworkClient.java
+++ src/test/java/hello/hellobasic/lifecycle/NetworkClient.java
@@ -1,11 +1,13 @@
 package hello.hellobasic.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
     public NetworkClient(){
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+
     }
 
     public void setUrl(String url) {
@@ -24,5 +26,22 @@ public class NetworkClient {
     // 서비스 종료시 호출
     public void disconnect(){
         System.out.println("close: " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // 스프링 전용 인터페이스 해당 코드가 스프링 전용 인터페이스에 의존
+        // 초기화, 소멸 메서드의 이름을 변경할수 없음
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        // 스프링 전용 인터페이스 해당 코드가 스프링 전용 인터페이스에 의존
+        // 초기화, 소멸 메서드의 이름을 변경할수 없음
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
