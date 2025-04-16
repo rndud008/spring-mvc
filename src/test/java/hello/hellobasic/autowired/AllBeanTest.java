e237a34f3bac0174c32791c6ae12d7db1ebe747b
rndud008 <rndud008@gmail.com>
2025-04-17 00:06:03 +0900

    [의존관계 자동 주입] - 조회한 빈이 모두 필요할 때, List, Map

---
+++ src/test/java/hello/hellobasic/autowired/AllBeanTest.java
@@ -0,0 +1,52 @@
+package hello.hellobasic.autowired;
+
+import hello.hellobasic.AutoAppConfig;
+import hello.hellobasic.dicount.DiscountPolicy;
+import hello.hellobasic.member.Grade;
+import hello.hellobasic.member.Member;
+import hello.hellobasic.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+public class AllBeanTest {
+    @Test
+    void findAllBean(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class,DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1l, "userA", Grade.VIP);
+
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+
+        Assertions.assertThat(discountService).isInstanceOf(DiscountService.class);
+        Assertions.assertThat(discountPrice).isEqualTo(1000);
+
+
+        int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+        Assertions.assertThat(rateDiscountPrice).isEqualTo(2000);
+    }
+
+    static class DiscountService{
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policies = " + policies);
+        }
+
+        public int discount(Member member, int price, String disCountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(disCountCode);
+
+            return discountPolicy.discount(member,price);
+        }
+    }
+}
