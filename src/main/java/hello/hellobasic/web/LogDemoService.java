3ace00dc21dae6f0f293edf5c19efa12fa7319e1
rndud008 <rndud008@gmail.com>
2025-04-17 23:10:28 +0900

    [빈 스코프] - request 스코프 예제 만들기

---
+++ src/main/java/hello/hellobasic/web/LogDemoService.java
@@ -0,0 +1,15 @@
+package hello.hellobasic.web;
+
+import hello.hellobasic.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
+}
