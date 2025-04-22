0d993fc9f8e5f1ab9af616ad3a039378efbd8ffb
rndud008 <rndud008@gmail.com>
2025-04-23 00:07:44 +0900

    [스프링 MVC - 웹페이지 만들기] -  상품 등록 처리 - @ModelAttribute

--- src/main/java/hello/hellobasic/web/basic/BasicItemController.java
+++ src/main/java/hello/hellobasic/web/basic/BasicItemController.java
@@ -19,29 +19,74 @@ public class BasicItemController {
     public final ItemRepository itemRepository;
 
     @GetMapping
-    public String items(Model model){
+    public String items(Model model) {
         List<Item> items = itemRepository.findAll();
-        model.addAttribute("items",items);
+        model.addAttribute("items", items);
         return "basic/items";
     }
 
     @GetMapping("/{itemId}")
-    public String item(@PathVariable("itemId") long itemId, Model model){
+    public String item(@PathVariable("itemId") long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
-        model.addAttribute("item",item);
+        model.addAttribute("item", item);
         return "basic/item";
 
     }
 
 
-
     @GetMapping("/add")
-    public String addForm(){
+    public String addForm() {
         return "basic/addForm";
     }
 
+
+    //    @PostMapping("/add")
+    public String addItmeV1(
+            @RequestParam("itemName") String itemName
+            , @RequestParam("price") int price
+            , @RequestParam("quantity") int quantity
+            , Model model
+    ) {
+
+        Item item = new Item(itemName, price, quantity);
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    //    @PostMapping("/add")
+    public String addItmeV2(
+            @ModelAttribute("item") Item item
+            , Model model
+    ) {
+        itemRepository.save(item);
+        //model.addAttribute("item",item); // 자동추가 ,생략가능
+        return "basic/item";
+    }
+
+    //    @PostMapping("/add")
+    public String addItmeV3(
+            @ModelAttribute Item item
+            , Model model
+    ) {
+        itemRepository.save(item);
+        //model.addAttribute("item",item); // 자동추가 ,생략가능
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItmeV4(
+            Item item
+            , Model model
+    ) {
+        itemRepository.save(item);
+        //model.addAttribute("item",item); // 자동추가 ,생략가능
+        return "basic/item";
+    }
+
     @PostConstruct
-    public void  init(){
+    public void init() {
         // 테스트 데이터 추가
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
