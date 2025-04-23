3bdb6a22800fe81fe518368a93175fa2a97f68cd
rndud008 <rndud008@gmail.com>
2025-04-24 00:53:28 +0900

    [타임리프 - 스프링 통합과 폼] - 요구사항 추가

--- src/main/java/hello/hellobasic/domain/item/Item.java
+++ src/main/java/hello/hellobasic/domain/item/Item.java
@@ -4,12 +4,19 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Data
 public class Item {
     private Long id;
     private String itemName;
     private Integer price;
     private Integer quantity;
+
+    private Boolean open; // 판매여부
+    private List<String> regions; // 등록지경
+    private ItemType itemType; // 상품 종류
+    private String deliveryCode; // 배송 방식
 
     public Item(){
     }
