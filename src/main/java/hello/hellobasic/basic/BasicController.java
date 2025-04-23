acd17b3b7c02ff29434adae0c5a579f9881540c4
rndud008 <rndud008@gmail.com>
2025-04-23 23:26:42 +0900

    [타임리프 - 기본기능] - 조건부 평가

--- src/main/java/hello/hellobasic/basic/BasicController.java
+++ src/main/java/hello/hellobasic/basic/BasicController.java
@@ -20,21 +20,21 @@ import java.util.Map;
 @RequestMapping("/basic")
 public class BasicController {
     @GetMapping("/text-basic")
-    public String textBasic(Model model){
-        model.addAttribute("data","Hello Spring!");
+    public String textBasic(Model model) {
+        model.addAttribute("data", "Hello Spring!");
         return "basic/text-basic";
     }
 
     @GetMapping("/text-unescaped")
-    public String textUnescaped(Model model){
-        model.addAttribute("data","Hello Spring!");
+    public String textUnescaped(Model model) {
+        model.addAttribute("data", "Hello Spring!");
         return "basic/text-unescaped";
     }
 
     @GetMapping("/variable")
-    public String variable(Model model){
-        User userA = new User("userA",10);
-        User userB = new User("userB",20);
+    public String variable(Model model) {
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
 
         List<User> list = new ArrayList<>();
         list.add(userA);
@@ -42,10 +42,10 @@ public class BasicController {
 
         Map<String, User> map = new HashMap<>();
         map.put("userA", userA);
-        map.put("userB",userA);
-        model.addAttribute("user",userA);
-        model.addAttribute("users",list);
-        model.addAttribute("userMap",map);
+        map.put("userB", userA);
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
 
         return "basic/variable";
     }
@@ -54,7 +54,7 @@ public class BasicController {
     public String basicObjects(Model model, HttpServletRequest request,
                                HttpServletResponse response, HttpSession session) {
 
-        session.setAttribute("sessionData","Hello Session");
+        session.setAttribute("sessionData", "Hello Session");
         model.addAttribute("request", request);
         model.addAttribute("response", response);
         model.addAttribute("servletContext", request.getServletContext());
@@ -68,67 +68,75 @@ public class BasicController {
         //recommended. In cases where they are really needed, they should be manually
         //added as context variables.
 
-    return "basic/basic-objects";
+        return "basic/basic-objects";
     }
 
     @GetMapping("/date")
-    public String date(Model model){
+    public String date(Model model) {
         model.addAttribute("localDateTime", LocalDateTime.now());
         return "basic/date";
     }
 
     @GetMapping("/link")
-    public String link(Model model){
-        model.addAttribute("param1","data1");
-        model.addAttribute("param2","data2");
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
         return "basic/link";
     }
 
     @GetMapping("/literal")
-    public String literal(Model model){
-        model.addAttribute("data","Spring!");
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!");
         return "basic/literal";
     }
 
     @GetMapping("operation")
-    public String operation(Model model){
-        model.addAttribute("nullData",null);
-        model.addAttribute("data","Spring!");
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
         return "basic/operation";
     }
 
     @GetMapping("/attribute")
-    public String attribute(){
+    public String attribute() {
 
         return "basic/attribute";
     }
+
     @GetMapping("/each")
-    public String each(Model model){
+    public String each(Model model) {
         addUsers(model);
         return "basic/each";
     }
 
-    private void addUsers(Model model){
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
+    }
+
+    private void addUsers(Model model) {
         List<User> list = new ArrayList<>();
-        list.add(new User("UserA",10));
-        list.add(new User("UserB",20));
-        list.add(new User("UserC",30));
-        model.addAttribute("users",list);
+        list.add(new User("UserA", 10));
+        list.add(new User("UserB", 20));
+        list.add(new User("UserC", 30));
+        model.addAttribute("users", list);
     }
 
 
     @Component("helloBean")
-    static class HelloBean{
-        public String hello(String data){
+    static class HelloBean {
+        public String hello(String data) {
             return "Hello " + data;
         }
     }
 
     @Data
-    static class User{
+    static class User {
         private String username;
         private int age;
-        public User(String username, int age){
+
+        public User(String username, int age) {
             this.username = username;
             this.age = age;
         }
