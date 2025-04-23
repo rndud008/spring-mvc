6e4121a530a6475067732d2531dc6c6ef2722dab
rndud008 <rndud008@gmail.com>
2025-04-23 23:17:56 +0900

    [타임리프 - 기본기능] - 속성 값 설정

--- src/main/java/hello/hellobasic/basic/BasicController.java
+++ src/main/java/hello/hellobasic/basic/BasicController.java
@@ -97,6 +97,12 @@ public class BasicController {
         return "basic/operation";
     }
 
+    @GetMapping("/attribute")
+    public String attribute(){
+
+        return "basic/attribute";
+    }
+
 
     @Component("helloBean")
     static class HelloBean{
