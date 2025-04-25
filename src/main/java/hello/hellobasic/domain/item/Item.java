637542c207a455ad9aec179390666b91d5102155
rndud008 <rndud008@gmail.com>
2025-04-25 19:47:47 +0900

    [검증2-Validation] - BeanValidation - 시작

--- src/main/java/hello/hellobasic/domain/item/Item.java
+++ src/main/java/hello/hellobasic/domain/item/Item.java
@@ -1,13 +1,23 @@
 package hello.hellobasic.domain.item;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
 @Data
 public class Item {
 
     private Long id;
+
+    @NotNull
     private String itemName;
+
+    @NotNull
+    @Range(min = 1_000, max = 1_000_000)
     private Integer price;
+
+    @NotNull
+    @Range( max = 9_999)
     private Integer quantity;
 
     public Item() {
