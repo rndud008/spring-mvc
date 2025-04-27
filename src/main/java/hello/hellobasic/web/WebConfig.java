1716e258646d390db7bd9ecf02f4042c09faf524
rndud008 <rndud008@gmail.com>
2025-04-27 20:09:28 +0900

    [로그인처리2 - 필터, 인터셉터] - 서블릿 필터 - 요청로그

---
+++ src/main/java/hello/hellobasic/web/WebConfig.java
@@ -0,0 +1,20 @@
+package hello.hellobasic.web;
+
+import hello.hellobasic.web.filter.LogFilter;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public FilterRegistrationBean logFilter() {
+
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LogFilter());
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.addUrlPatterns("/*");
+        return filterFilterRegistrationBean;
+    }
+}
