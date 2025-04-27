ffe93b5fd9cd1c2ee44bcb944a9c0b534cfe0cc7
rndud008 <rndud008@gmail.com>
2025-04-27 14:38:38 +0900

    [로그인처리1 - 쿠키, 세션] -  프로젝트생성

---
+++ src/main/java/hello/hellobasic/web/item/form/ItemSaveForm.java
@@ -0,0 +1,24 @@
+package hello.hellobasic.web.item.form;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+
+@Data
+public class ItemSaveForm {
+
+    @NotBlank
+    private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
+    private Integer price;
+
+    @NotNull
+    @Max(value = 9_999)
+    private Integer quantity;
+
+}
