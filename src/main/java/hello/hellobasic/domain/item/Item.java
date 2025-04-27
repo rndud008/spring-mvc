79604373243ca1f01c57ca323c48cee3722f94a3
rndud008 <rndud008@gmail.com>
2025-04-27 12:40:27 +0900

    [검증2-Validation] - BeanValidation - 오브젝트 오류

--- src/main/java/hello/hellobasic/domain/item/Item.java
+++ src/main/java/hello/hellobasic/domain/item/Item.java
@@ -4,8 +4,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 @Data
+// @ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원 넘게 입력해주세요")
+// 자바 15 이후로 Nashorn 엔진이 제거됐고 자바 17 이상을 쓰면 기본으로 JavaScript 엔진이 아예 없으므로 ScriptEvaluatorNotFoundException 발생
 public class Item {
 
     private Long id;
