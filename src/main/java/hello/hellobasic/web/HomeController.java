0111ee1f56fc79aa68b179ee409b7717b06eac57
rndud008 <rndud008@gmail.com>
2025-04-27 16:02:29 +0900

    [로그인처리1 - 쿠키, 세션] - 로그인 처리하기 - 서블릿 HTTP 세션2

--- src/main/java/hello/hellobasic/web/HomeController.java
+++ src/main/java/hello/hellobasic/web/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Slf4j
 @Controller
@@ -55,7 +56,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model){
 
         // 세션 관리자에 저장된 회원정보 조회
@@ -65,6 +66,17 @@ public class HomeController {
         }
 
         Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        if (loginMember == null){
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model){
 
         if (loginMember == null){
             return "home";
