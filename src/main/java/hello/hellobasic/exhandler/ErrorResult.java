7101ddb366f52c13702ecf154b5d5a0009ef3488
rndud008 <rndud008@gmail.com>
2025-04-28 23:31:37 +0900

    [API 예외 처리] @ExceptionHandler

---
+++ src/main/java/hello/hellobasic/exhandler/ErrorResult.java
@@ -0,0 +1,11 @@
+package hello.hellobasic.exhandler;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResult {
+    private String code;
+    private String message;
+}
