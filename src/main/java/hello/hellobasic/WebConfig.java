ed7d5a7470ffd18f668bb785923ac1af3309f79b
rndud008 <rndud008@gmail.com>
2025-04-29 22:35:27 +0900

    [스프링 타입 컨버터] 포맷터를 지원 하는 컨버전 서비스

--- src/main/java/hello/hellobasic/WebConfig.java
+++ src/main/java/hello/hellobasic/WebConfig.java
@@ -4,6 +4,7 @@ import hello.hellobasic.converter.IntegerToStringConverter;
 import hello.hellobasic.converter.IpPortToStringConverter;
 import hello.hellobasic.converter.StringToIntegerConverter;
 import hello.hellobasic.converter.StringToIpPortConvert;
+import hello.hellobasic.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,9 +14,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        // 컨버터가 포맷터보다 우선순위가 높기 때문에 주석처리
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
+
         registry.addConverter(new StringToIpPortConvert());
         registry.addConverter(new IpPortToStringConverter());
+
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
