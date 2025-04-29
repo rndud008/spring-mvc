99b713dd6ed5136e81823d1bdaa91c6a131b85cf
rndud008 <rndud008@gmail.com>
2025-04-29 21:30:06 +0900

    [스프링 타입 컨버터] 타입 컨버터 - Converter

---
+++ src/main/java/hello/hellobasic/converter/StringToIntegerConverter.java
@@ -0,0 +1,15 @@
+package hello.hellobasic.converter;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.converter.Converter;
+
+@Slf4j
+public class StringToIntegerConverter implements Converter<String, Integer> {
+
+    @Override
+    public Integer convert(String source) {
+        log.info("converter source={}",source);
+        return Integer.parseInt(source);
+    }
+}
---
+++ src/test/java/hello/hellobasic/converter/ConverterTest.java
@@ -0,0 +1,39 @@
+package hello.hellobasic.converter;
+
+import hello.hellobasic.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ConverterTest {
+
+    @Test
+    void stringToInteger(){
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        Integer result = converter.convert("10");
+        Assertions.assertThat(result).isEqualTo(10);
+    }
+
+    @Test
+    void integerToString(){
+        IntegerToStringConverter converter = new IntegerToStringConverter();
+        String result = converter.convert(10);
+        Assertions.assertThat(result).isEqualTo("10");
+    }
+
+    @Test
+    void stringToIpPort(){
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        IpPort source = new IpPort("127.0.0.1",8080);
+        String result = converter.convert(source);
+        Assertions.assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+
+    @Test
+    void ipPortToString(){
+        StringToIpPortConvert convert = new StringToIpPortConvert();
+        String source = "127.0.0.1:8080";
+        IpPort result = convert.convert(source);
+        Assertions.assertThat(result).isEqualTo(new IpPort("127.0.0.1",8080));
+
+    }
+}
