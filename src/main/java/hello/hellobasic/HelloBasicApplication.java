33eb7a49e488477a33f7c2a071a3dd635f201fd5
rndud008 <rndud008@gmail.com>
2025-05-01 23:28:37 +0900

    [데이터 접근 기술 - 스프링JdbcTemplate] - JdbcTemplate 적용3 - 구성과 실행

--- src/main/java/hello/hellobasic/HelloBasicApplication.java
+++ src/main/java/hello/hellobasic/HelloBasicApplication.java
@@ -1,5 +1,6 @@
 package hello.hellobasic;
 
+import hello.hellobasic.config.JdbcTemplateV1Config;
 import hello.hellobasic.config.MemoryConfig;
 import hello.hellobasic.repository.ItemRepository;
 import org.springframework.boot.SpringApplication;
@@ -9,7 +10,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 
-@Import(MemoryConfig.class)
+//@Import(MemoryConfig.class)
+@Import(JdbcTemplateV1Config.class)
 @SpringBootApplication(scanBasePackages = "hello.hellobasic.web")
 public class HelloBasicApplication {
 
