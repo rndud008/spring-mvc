91e7c3cc4567c745f632af771d662f6014fffb19
rndud008 <rndud008@gmail.com>
2025-05-01 20:35:40 +0900

    [스프링과 문제해결- 예외처리, 반복] - 체크예외와 인터페이스

---
+++ src/main/java/hello/hellobasic/repository/MemberRepositoryEx.java
@@ -0,0 +1,12 @@
+package hello.hellobasic.repository;
+
+import hello.hellobasic.domain.Member;
+
+import java.sql.SQLException;
+
+public interface MemberRepositoryEx {
+    Member save(Member member) throws SQLException;
+    Member findById(String memberId) throws SQLException;
+    void update(String memberId, int money) throws SQLException;
+    void delete(String memberId) throws SQLException;
+}
