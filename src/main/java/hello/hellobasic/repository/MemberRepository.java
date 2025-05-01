1695d248ca8046a6fbd555130937368b5f3c3142
rndud008 <rndud008@gmail.com>
2025-05-01 20:45:55 +0900

    [스프링과 문제해결- 예외처리, 반복] - 런타임 예외 적용

---
+++ src/main/java/hello/hellobasic/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package hello.hellobasic.repository;
+
+import hello.hellobasic.domain.Member;
+
+import java.sql.SQLException;
+
+public interface MemberRepository {
+    Member save(Member member) ;
+    Member findById(String memberId) ;
+    void update(String memberId, int money) ;
+    void delete(String memberId) ;
+}
---
+++ src/main/java/hello/hellobasic/service/MemberServiceV4.java
@@ -0,0 +1,41 @@
+package hello.hellobasic.service;
+
+import hello.hellobasic.domain.Member;
+import hello.hellobasic.repository.MemberRepository;
+import hello.hellobasic.repository.MemberRepositoryV3;
+import hello.hellobasic.repository.MemberRepositoryV4_1;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+
+@Slf4j
+
+public class MemberServiceV4 {
+
+    private final MemberRepository memberRepository;
+
+    public MemberServiceV4(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money)  {
+        bizLogic(fromId, toId, money);
+    }
+
+    private void bizLogic(String fromId, String toId, int money)   {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private static void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
