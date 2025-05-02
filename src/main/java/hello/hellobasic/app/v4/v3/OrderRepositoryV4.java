e22245166ed066b6e7b7383fbaf37bc9c0311a76
rndud008 <rndud008@gmail.com>
2025-05-03 00:41:59 +0900

    [템플릿 메서드 패턴과 콜백 패턴] - 템플릿 메서드 패턴 - 적용 1

---
+++ src/main/java/hello/hellobasic/app/v4/v3/OrderRepositoryV4.java
@@ -0,0 +1,40 @@
+package hello.hellobasic.app.v4.v3;
+
+import hello.hellobasic.trace.TraceStatus;
+import hello.hellobasic.trace.logtrace.LogTrace;
+import hello.hellobasic.trace.trace.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV4 {
+
+    private final LogTrace trace;
+
+    public void save( String itemId) {
+
+        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
+            @Override
+            protected Void call() {
+                if (itemId.equals("ex")) {
+                    throw new IllegalStateException("예외 발생!");
+                }
+                sleep(1000);
+                return null;
+            }
+        };
+        template.execute("OrderRepositoryV4.save()");
+
+
+
+    }
+
+    private static void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
