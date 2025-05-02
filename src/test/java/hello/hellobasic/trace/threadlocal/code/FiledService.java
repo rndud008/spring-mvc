3362e8a29d77ac33796737297a98d42b85ca4837
rndud008 <rndud008@gmail.com>
2025-05-02 23:39:10 +0900

    [쓰레드 로컬 - ThreadLocal] - 동시성 문제 - 예제 코드

---
+++ src/test/java/hello/hellobasic/trace/threadlocal/code/FiledService.java
@@ -0,0 +1,23 @@
+package hello.hellobasic.trace.threadlocal.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class FiledService {
+    private String nameStore;
+    public String logic(String name){
+        log.info("저장 name ={} -> namestore={}",name, nameStore);
+        nameStore = name;
+        sleep(1000);
+        log.info("조회 nameStore={}",nameStore);
+        return nameStore;
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
