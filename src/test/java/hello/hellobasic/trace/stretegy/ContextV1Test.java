a7b6a090ffd030e63df3961fc35750e6482eaf46
rndud008 <rndud008@gmail.com>
2025-05-03 01:09:08 +0900

    [템플릿 메서드 패턴과 콜백 패턴] - 전략 패턴 - 예제 2

--- src/test/java/hello/hellobasic/trace/stretegy/ContextV1Test.java
+++ src/test/java/hello/hellobasic/trace/stretegy/ContextV1Test.java
@@ -1,6 +1,7 @@
 package hello.hellobasic.trace.stretegy;
 
 import hello.hellobasic.trace.stretegy.code.strategy.ContextV1;
+import hello.hellobasic.trace.stretegy.code.strategy.Strategy;
 import hello.hellobasic.trace.stretegy.code.strategy.StrategyLogic1;
 import hello.hellobasic.trace.stretegy.code.strategy.StrategyLogic2;
 import hello.hellobasic.trace.template.code.AbstractTemplate;
@@ -13,29 +14,29 @@ import org.junit.jupiter.api.Test;
 public class ContextV1Test {
 
     @Test
-    void strategyV0(){
+    void strategyV0() {
         logic1();
         logic2();
     }
 
-    private void logic1(){
+    private void logic1() {
         long startTime = System.currentTimeMillis();
         log.info("비즈니스 로직1 실행");
         long endTime = System.currentTimeMillis();
-        long resultTime = endTime- startTime;
-        log.info("resultTime ={}",resultTime);
+        long resultTime = endTime - startTime;
+        log.info("resultTime ={}", resultTime);
     }
 
-    private void logic2(){
+    private void logic2() {
         long startTime = System.currentTimeMillis();
         log.info("비즈니스 로직2 실행");
         long endTime = System.currentTimeMillis();
-        long resultTime = endTime- startTime;
-        log.info("resultTime ={}",resultTime);
+        long resultTime = endTime - startTime;
+        log.info("resultTime ={}", resultTime);
     }
 
     @Test
-    void strategyV1(){
+    void strategyV1() {
         StrategyLogic1 strategyLogic1 = new StrategyLogic1();
         ContextV1 contextV1 = new ContextV1(strategyLogic1);
         contextV1.execute();
@@ -44,5 +45,66 @@ public class ContextV1Test {
         ContextV1 contextV2 = new ContextV1(strategyLogic2);
         contextV2.execute();
     }
+
+    @Test
+    void strategyV2() {
+        Strategy strategyLogic1 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 1 실행");
+            }
+        };
+
+        ContextV1 con = new ContextV1(strategyLogic1);
+        log.info("strategyLogic1={}", strategyLogic1.getClass());
+        con.execute();
+
+        Strategy strategyLogic2 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 2 실행");
+            }
+        };
+
+        ContextV1 con2 = new ContextV1(strategyLogic2);
+        log.info("strategyLogic2={}", strategyLogic2.getClass());
+        con2.execute();
+
+    }
+
+    @Test
+    void strategyV3() {
+
+        ContextV1 con = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 1 실행");
+            }
+        });
+        con.execute();
+
+
+        ContextV1 con2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 2 실행");
+            }
+        });
+        con2.execute();
+
+    }
+
+
+    @Test
+    void strategyV4() {
+
+        ContextV1 con = new ContextV1(() -> log.info("비즈니스 로직 1 실행"));
+        con.execute();
+
+        ContextV1 con2 = new ContextV1(() -> log.info("비즈니스 로직 2 실행"));
+        con2.execute();
+
+    }
+
 
 }
