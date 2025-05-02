a7a039f9fd71b063aa448a0ef2a35a305924bafd
rndud008 <rndud008@gmail.com>
2025-05-02 10:28:10 +0900

    [데이터 접근 기술 - MyBatis] - MyBastis적용1 - 기본

---
+++ src/main/java/hello/hellobasic/repository/mybatis/ItemMapper.java
@@ -0,0 +1,22 @@
+package hello.hellobasic.repository.mybatis;
+
+import hello.hellobasic.domain.Item;
+import hello.hellobasic.repository.ItemSearchCond;
+import hello.hellobasic.repository.ItemUpdateDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+@Mapper
+public interface ItemMapper {
+
+    void save(Item item);
+
+    void update(@Param("id") Long id, @Param("updateParma")ItemUpdateDto itemUpdateDto);
+
+    Optional<Item> findById(Long id);
+    List<Item> findAll(ItemSearchCond itemSearchCond);
+
+}
