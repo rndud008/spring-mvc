75322aead6bb75d40e3bfb0f15c1260da148e40c
rndud008 <rndud008@gmail.com>
2025-05-03 01:02:12 +0900

    [템플릿 메서드 패턴과 콜백 패턴] - 전략 패턴 - 예제 1

--- src/test/java/hello/hellobasic/trace/stretegy/ContextV1Test.java
+++ src/test/java/hello/hellobasic/trace/stretegy/ContextV1Test.java
@@ -1,5 +1,8 @@
 package hello.hellobasic.trace.stretegy;
 
+import hello.hellobasic.trace.stretegy.code.strategy.ContextV1;
+import hello.hellobasic.trace.stretegy.code.strategy.StrategyLogic1;
+import hello.hellobasic.trace.stretegy.code.strategy.StrategyLogic2;
 import hello.hellobasic.trace.template.code.AbstractTemplate;
 import hello.hellobasic.trace.template.code.SubClassLogic1;
 import hello.hellobasic.trace.template.code.SubClassLogic2;
@@ -31,5 +34,15 @@ public class ContextV1Test {
         log.info("resultTime ={}",resultTime);
     }
 
+    @Test
+    void strategyV1(){
+        StrategyLogic1 strategyLogic1 = new StrategyLogic1();
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        contextV1.execute();
+
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        contextV2.execute();
+    }
 
 }
---
+++ src/test/java/hello/hellobasic/trace/stretegy/code/strategy/Strategy.java
@@ -0,0 +1,5 @@
+package hello.hellobasic.trace.stretegy.code.strategy;
+
+public interface Strategy {
+    void call();
+}
