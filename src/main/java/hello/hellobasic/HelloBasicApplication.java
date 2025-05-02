c306591e00235183f3069ad13506aaa9e5e0d997
rndud008 <rndud008@gmail.com>
2025-05-02 09:44:02 +0900

    [데이터 접근 기술 - 테스트] - 테스트 - 임베디드 모드 DB

--- src/main/java/hello/hellobasic/HelloBasicApplication.java
+++ src/main/java/hello/hellobasic/HelloBasicApplication.java
@@ -5,11 +5,15 @@ import hello.hellobasic.config.JdbcTemplateV2Config;
 import hello.hellobasic.config.JdbcTemplateV3Config;
 import hello.hellobasic.config.MemoryConfig;
 import hello.hellobasic.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 
 //@Import(MemoryConfig.class)
@@ -17,6 +21,7 @@ import org.springframework.context.annotation.Profile;
 //@Import(JdbcTemplateV2Config.class)
 @Import(JdbcTemplateV3Config.class)
 @SpringBootApplication(scanBasePackages = "hello.hellobasic.web")
+@Slf4j
 public class HelloBasicApplication {
 
     public static void main(String[] args) {
@@ -27,5 +32,17 @@ public class HelloBasicApplication {
     @Profile("local")
     public TestDataInit testDataInit(ItemRepository itemRepository) {
         return new TestDataInit(itemRepository);
+    }
+
+    @Bean
+    @Profile("test")
+    public DataSource dataSource(){
+        log.info("메모리 데이터베이스 초기화");
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        return dataSource;
     }
 }
