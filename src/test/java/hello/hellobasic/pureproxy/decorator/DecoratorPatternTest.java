aa36f35855b2cb0e15776be3b46ece55fc3afc78
rndud008 <rndud008@gmail.com>
2025-05-03 10:32:08 +0900

    [프록시 패턴과 데코레이터 패턴] - 데코레이터 패턴 - 예제 코드2

--- src/test/java/hello/hellobasic/pureproxy/decorator/DecoratorPatternTest.java
+++ src/test/java/hello/hellobasic/pureproxy/decorator/DecoratorPatternTest.java
@@ -2,6 +2,7 @@ package hello.hellobasic.pureproxy.decorator;
 
 import hello.hellobasic.pureproxy.decorator.code.Component;
 import hello.hellobasic.pureproxy.decorator.code.DecoratorPatternClient;
+import hello.hellobasic.pureproxy.decorator.code.MessageDecorator;
 import hello.hellobasic.pureproxy.decorator.code.RealComponent;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,17 @@ public class DecoratorPatternTest {
     void noDecorator(){
         Component realComponent = new RealComponent();
         DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void decorator(){
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
 
         client.execute();
         client.execute();
