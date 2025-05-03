15133b1f0243afc92dcbc01b3f20d774962f2fbe
rndud008 <rndud008@gmail.com>
2025-05-03 16:50:39 +0900

    [스프링 AOP 구현] - 스프링 AOP 구현3 - 어드바이스 추가

--- src/test/java/hello/hellobasic/AopTest.java
+++ src/test/java/hello/hellobasic/AopTest.java
@@ -4,6 +4,7 @@ import hello.hellobasic.order.OrderRepository;
 import hello.hellobasic.order.OrderService;
 import hello.hellobasic.order.aop.AspectV1;
 import hello.hellobasic.order.aop.AspectV2;
+import hello.hellobasic.order.aop.AspectV3;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,7 +15,7 @@ import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
-@Import(AspectV2.class)
+@Import(AspectV3.class)
 public class AopTest {
 
     @Autowired
