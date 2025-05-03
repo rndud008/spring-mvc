a8a26d5ba9415d3552d6b55e91f5074dc9063eac
rndud008 <rndud008@gmail.com>
2025-05-03 12:53:28 +0900

    [동적 프록시 기술] - CGLIB - 예제코드

---
+++ src/test/java/hello/hellobasic/cglib/CglibTest.java
@@ -0,0 +1,24 @@
+package hello.hellobasic.cglib;
+
+import hello.hellobasic.cglib.code.TimeMethodInterceptor;
+import hello.hellobasic.common.service.ConcreteService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.cglib.proxy.Enhancer;
+
+@Slf4j
+public class CglibTest {
+    @Test
+    void cglib(){
+        ConcreteService target = new ConcreteService();
+
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(ConcreteService.class);
+        enhancer.setCallback(new TimeMethodInterceptor(target));
+        ConcreteService proxy = (ConcreteService) enhancer.create();
+        log.info("targetClass={}",target.getClass());
+        log.info("proxyClass={}",proxy.getClass());
+
+        proxy.call();
+    }
+}
---
+++ src/test/java/hello/hellobasic/common/service/ServiceInterface.java
@@ -0,0 +1,6 @@
+package hello.hellobasic.common.service;
+
+public interface ServiceInterface {
+    void save();
+    void find();
+}
