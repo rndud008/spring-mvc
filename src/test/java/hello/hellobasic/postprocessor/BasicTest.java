e759cfec1a0d27bed1f1206a09ddd1ac3be142b7
rndud008 <rndud008@gmail.com>
2025-05-03 14:15:56 +0900

    [빈 후처리기] - 빈 후처리기 - 예제코드 1

---
+++ src/test/java/hello/hellobasic/postprocessor/BasicTest.java
@@ -0,0 +1,48 @@
+package hello.hellobasic.postprocessor;
+
+import com.jayway.jsonpath.internal.function.json.Append;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+public class BasicTest {
+    @Test
+    void basicConfig(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(BasicConfig.class);
+
+        // A는 빈으로 등록된다
+        A a = ac.getBean("beanA", A.class);
+
+        a.helloA();
+
+        // B 는 스프링 빈으로 등록되지 않는다
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class, ()-> ac.getBean("beanB",B.class));
+
+    }
+
+    @Configuration
+    static class BasicConfig{
+        @Bean(name = "beanA")
+        public A a(){
+            return  new A();
+        }
+    }
+
+    static class A{
+        public void helloA(){
+            log.info("hello A");
+        }
+    }
+
+    static class B{
+        public void helloB(){
+            log.info("hello B");
+        }
+    }
+}
