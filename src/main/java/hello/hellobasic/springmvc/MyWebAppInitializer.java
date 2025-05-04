f067abbfc4f51508014926f8b26836bd4f3c87d8
rndud008 <rndud008@gmail.com>
2025-05-04 16:11:39 +0900

    [스프링 웹 MVC 기본] - 아키텍처 이해

--- src/main/java/hello/hellobasic/springmvc/MyWebAppInitializer.java
+++
@@ -1,8 +0,0 @@
-package hello.hellobasic.springmvc;
-
-import jakarta.servlet.ServletContext;
-
-public interface MyWebAppInitializer {
-    // 실제 초기화에 필요한 메서드 정의
-    void onStartup(ServletContext servletContext);
-}
