5a4027177f14850e7dbfcbbdaf1fe45dc8804f1a
rndud008 <rndud008@gmail.com>
2025-05-05 01:13:04 +0900

    [스프링 웹 MVC 활용 - HandlerAdapter] - HttpEntity_RequestEntity

--- src/main/java/hello/hellobasic/User.java
+++ src/main/java/hello/hellobasic/User.java
@@ -1,22 +1,14 @@
 package hello.hellobasic;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.web.bind.annotation.BindParam;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
-//@AllArgsConstructor
 public class User {
+
     private String username;
     private String email;
-
-    public User(@BindParam("my-name") String username, @BindParam("my-email") String email) {
-        this.username = username;
-        this.email = email;
-
-    }
-
 }
