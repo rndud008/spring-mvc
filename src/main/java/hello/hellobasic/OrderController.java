973924e758e0c91841582bcbee720372d5e1b5d0
rndud008 <rndud008@gmail.com>
2025-05-05 16:32:22 +0900

    [스프링 웹 MVC 검증 - Validation] -  BindingResult 와 MessageSource 연동 2

--- src/main/java/hello/hellobasic/OrderController.java
+++ src/main/java/hello/hellobasic/OrderController.java
@@ -1,4 +1,5 @@
 package hello.hellobasic;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -17,27 +18,49 @@ public class OrderController {
         model.addAttribute("order", new Order());
         return "orderForm";
     }
-    @PostMapping("/order")
+
+    /*@PostMapping("/order")
     public String submitOrder(@ModelAttribute("order") Order order, BindingResult bindingResult) {
 
         if (!StringUtils.hasText(order.getItem())) {
-            bindingResult.addError(new FieldError("order", "item", order.getItem(),
-                    false, new String[]{"required.order.item"}, null,  ""));
+            bindingResult.rejectValue("item", "required", null, "상품명은 필수입니다1");
         }
-
         if (order.getPrice() < 1000 || order.getPrice() > 1_000_000) {  // 가격 범위 제한: 1 ~ 1,000,000)
-            bindingResult.addError(new FieldError("order", "price",  order.getPrice(),
-                    false, new String[]{"range.order.price"}, new Object[]{100, 10000}, ""));
+            bindingResult.rejectValue("price", "range", new Object[]{100, 10000}, "가격은 100 이상 10000 이하여만 합니다1.");
         }
-
         // 객체 수준 오류
         if (!StringUtils.hasText(order.getItem()) && (order.getPrice() < 1000)) {
-            bindingResult.addError(new ObjectError("order", "유효하지 않은 상품명입니다."));
+            bindingResult.reject("required", "유효하지 않은 상품명입니다.");
         }
 
         if (bindingResult.hasErrors()) {
             return "orderForm";
         }
         return "orderResult";
+    }*/
+
+    @PostMapping("/order")
+    public String submitOrder(@ModelAttribute("order") Order order, BindingResult bindingResult) {
+
+
+        if (!StringUtils.hasText(order.getItem())) {
+            bindingResult.rejectValue("item", "required");
+        }
+        if (order.getItem().length() > 30) {
+            bindingResult.rejectValue("item", "max");
+        }
+
+        if (order.getPrice() < 1000) {
+            bindingResult.rejectValue("price", "min", new Object[]{1000}, "주문가격은 1000 원 이상이어야 합니다.");
+        } else if (order.getPrice() > 1000000) {
+            bindingResult.rejectValue("price", "max", new Object[]{1000000}, "주문가격은 1000000 원 이하여야 합니다.");
+        }
+
+
+        if (bindingResult.hasErrors()) {
+            return "orderForm";
+        }
+        return "orderResult";
     }
+
 }
--- src/test/java/hello/hellobasic/HelloBasicApplicationTests.java
+++ src/test/java/hello/hellobasic/HelloBasicApplicationTests.java
@@ -7,9 +7,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.validation.DefaultMessageCodesResolver;
 
 import java.lang.reflect.Field;
 import java.text.ParseException;
+import java.util.Arrays;
 import java.util.Locale;
 
 @SpringBootTest
@@ -17,7 +19,17 @@ class HelloBasicApplicationTests {
 
     @Test
     void contextLoads() throws ParseException {
+        DefaultMessageCodesResolver resolver = new DefaultMessageCodesResolver();
+        String[] objectErrorCodes = resolver.resolveMessageCodes("required","user");
+        Arrays.stream(objectErrorCodes).forEach(System.out::println);
+        System.out.println("objectErrorCodes = " + objectErrorCodes);
 
+        String[] filedErrorCodes = resolver.resolveMessageCodes("required","user","age",Integer.class);
+        Arrays.stream(filedErrorCodes).forEach(System.out::println);
+        System.out.println("filedErrorCodes = " + filedErrorCodes);
+
+        
+        
     }
 
 
