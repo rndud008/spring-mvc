46a9f4424f9fa793e3c87412e8d67c403adcf38f
rndud008 <rndud008@gmail.com>
2025-05-05 16:03:39 +0900

    [스프링 웹 MVC 검증 - Validation] -  BindingResult 와 MessageSource 연동 1

--- src/main/java/hello/hellobasic/Order.java
+++ src/main/java/hello/hellobasic/Order.java
@@ -6,5 +6,4 @@ import lombok.Data;
 public class Order {
     private String item;
     private int price;
-    private int quantity;
 }
--- src/main/java/hello/hellobasic/OrderController.java
+++ src/main/java/hello/hellobasic/OrderController.java
@@ -1,5 +1,4 @@
 package hello.hellobasic;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -19,27 +18,26 @@ public class OrderController {
         return "orderForm";
     }
     @PostMapping("/order")
-    public String submitOrder(@ModelAttribute("order") Order order, BindingResult result) {
+    public String submitOrder(@ModelAttribute("order") Order order, BindingResult bindingResult) {
 
-        // 필드 오류 검증 (productName)
         if (!StringUtils.hasText(order.getItem())) {
-            result.addError(new FieldError("order", "item", order.getItem(), false, null, null,  "상품명은 필수입니다."));
+            bindingResult.addError(new FieldError("order", "item", order.getItem(),
+                    false, new String[]{"required.order.item"}, null,  ""));
         }
-        // 필드 오류 검증 (quantity)
-        if (order.getQuantity() <= 10) {
-            result.addError(new FieldError("order", "quantity", order.getQuantity(), false, null, null, "수량은 10보다 커야 합니다."));
-        }
-        if (order.getPrice() < 1000 || order.getPrice() > 1_000_000) {  // 가격 범위 제한: 1 ~ 1,000,000)
-            result.addError(new FieldError("order", "price",  order.getPrice(), false, null, null, "가격은 1000 이상 1,000,000 이하여야 합니다."));
-        }
-        // 객체 수준 오류
-        if (!StringUtils.hasText(order.getItem()) && (order. getQuantity() <= 10)) {
-            result.addError(new ObjectError("order", "유효하지 않은 상품명입니다."));
-        }
-        if (result.hasErrors()){
-            return "orderForm";  // 오류가 있으면 주문 폼으로 돌아가기
-        }
-        return "orderForm";
-    }
 
+        if (order.getPrice() < 1000 || order.getPrice() > 1_000_000) {  // 가격 범위 제한: 1 ~ 1,000,000)
+            bindingResult.addError(new FieldError("order", "price",  order.getPrice(),
+                    false, new String[]{"range.order.price"}, new Object[]{100, 10000}, ""));
+        }
+
+        // 객체 수준 오류
+        if (!StringUtils.hasText(order.getItem()) && (order.getPrice() < 1000)) {
+            bindingResult.addError(new ObjectError("order", "유효하지 않은 상품명입니다."));
+        }
+
+        if (bindingResult.hasErrors()) {
+            return "orderForm";
+        }
+        return "orderResult";
+    }
 }
