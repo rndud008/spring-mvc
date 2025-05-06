731c9891ced3d2e36da936166db99c84b9dbfe86
rndud008 <rndud008@gmail.com>
2025-05-06 13:52:57 +0900

    [스프링 웹 MVC 핸들러 메서드 실행구조 이해] - 메서드 파리미터 실행 구조 이해

--- src/main/java/hello/hellobasic/MyController.java
+++ src/main/java/hello/hellobasic/MyController.java
@@ -3,59 +3,22 @@ package hello.hellobasic;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.context.request.ServletWebRequest;
-import org.springframework.web.method.support.*;
-import org.springframework.web.servlet.mvc.method.annotation.ServletInvocableHandlerMethod;
-
-import java.lang.reflect.Method;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequiredArgsConstructor
 public class MyController {
 
-    private final MyService myService;
-
-    @GetMapping("/invokeService1")
-    public void invokeService1(HttpServletRequest request, HttpServletResponse response) throws Exception {
-
-        Method serviceMethod= myService.getClass().getMethod("processRequest", User.class);
-        InvocableHandlerMethod invocableHandlerMethod = new InvocableHandlerMethod(myService, serviceMethod);
-
-        HandlerMethodArgumentResolverComposite resolver = new HandlerMethodArgumentResolverComposite();
-        resolver.addResolvers(new MyArgumentResolver());
-        invocableHandlerMethod.setHandlerMethodArgumentResolvers(resolver);
-
-        ServletWebRequest servletWebRequest = new ServletWebRequest(request, response);
-        ModelAndViewContainer modelAndViewContainer = new ModelAndViewContainer();
-
-        Object result = invocableHandlerMethod.invokeForRequest(servletWebRequest, modelAndViewContainer);
-        response.getWriter().write(result.toString());
+    @GetMapping("/dynamic1")
+    public User dynamic1(@RequestParam("username") String name, @ModelAttribute User user, Model model) throws Exception {
+        return user;
     }
 
-    @GetMapping("/invokeService2")
-    public void invokeService2(HttpServletRequest request, HttpServletResponse response) throws Exception {
-
-        Method serviceMethod= myService.getClass().getMethod("processRequest", User.class);
-        ServletInvocableHandlerMethod invocableHandlerMethod = new ServletInvocableHandlerMethod(myService, serviceMethod);
-
-        HandlerMethodArgumentResolverComposite resolver = new HandlerMethodArgumentResolverComposite();
-        resolver.addResolvers(new MyArgumentResolver());
-
-        HandlerMethodReturnValueHandlerComposite returnValueHandler = new HandlerMethodReturnValueHandlerComposite();
-        returnValueHandler.addHandler(new MyReturnValueHandler());
-
-        invocableHandlerMethod.setHandlerMethodArgumentResolvers(resolver);
-        invocableHandlerMethod.setHandlerMethodReturnValueHandlers(returnValueHandler);
-
-        ServletWebRequest servletWebRequest = new ServletWebRequest(request, response);
-        ModelAndViewContainer modelAndViewContainer = new ModelAndViewContainer();
-
-        invocableHandlerMethod.invokeAndHandle(servletWebRequest,modelAndViewContainer);
-        if(modelAndViewContainer.containsAttribute("result")){
-            response.getWriter().write(modelAndViewContainer.getModel().get("result").toString());
-        }
+    @GetMapping("/dynamic2")
+    public String dynamic2(HttpServletRequest request, HttpServletResponse response,  Model model) throws Exception {
+        return "springmvc";
     }
 }
