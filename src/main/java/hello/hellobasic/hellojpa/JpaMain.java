a3dec6356d176b806970872adc596178231bfa47
rndud008 <rndud008@gmail.com>
2025-05-12 22:32:19 +0900

    [자바 ORM 표준 JPA 프로그래밍 - 기본편] - JPA 시작하기 - 애플리케이션 개발

---
+++ src/main/java/hello/hellobasic/hellojpa/JpaMain.java
@@ -0,0 +1,49 @@
+package hello.hellobasic.hellojpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+
+//            생성
+//            Member member = new Member();
+//            member.setName("HelloB");
+//            member.setId(2l);
+//            em.persist(member);
+
+//            수정 + 조회
+//            Member findMember = em.find(Member.class, 1l);
+//            findMember.setName("HelloJPA");
+
+//            조회
+//            List<Member> result = em.createQuery("select m from Member m", Member.class)
+//                    .setFirstResult(5)
+//                    .setMaxResults(7)
+//                    .getResultList();
+//            for (Member member : result){
+//                System.out.println("member.getName() = " + member.getName());
+//            }
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+        emf.close();
+
+    }
+}
