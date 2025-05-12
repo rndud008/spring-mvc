1c86bd45ccfcbb88f85c21045b82643b03eb05ca
rndud008 <rndud008@gmail.com>
2025-05-13 00:48:14 +0900

    [자바 ORM 표준 JPA 프로그래밍 - 기본편] - 엔티티 매핑 - 필드와 컬럼 매핑

--- src/main/java/hello/hellobasic/hellojpa/Member.java
+++ src/main/java/hello/hellobasic/hellojpa/Member.java
@@ -1,15 +1,55 @@
 package hello.hellobasic.hellojpa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 @Getter
 @Setter
 public class Member {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+
+    @Column(name = "name", nullable = false)
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
+
+    @Lob
+    private String description;
+
+    @Transient
+    private int temp;
+
+    //     create table Member (
+    //        age integer,
+    //        testLocalDate date,
+    //        createdDate timestamp(6),
+    //        id bigint not null,
+    //        lastModifiedDate timestamp(6),
+    //        testLocalDateTime timestamp(6),
+    //        name varchar(255) not null,
+    //        description clob,
+    //        roleType enum ('ADMIN','USER'),
+    //        primary key (id)
+    //    )
+
 }
---
+++ src/main/java/hello/hellobasic/hellojpa/RoleType.java
@@ -0,0 +1,5 @@
+package hello.hellobasic.hellojpa;
+
+public enum RoleType {
+    USER,ADMIN
+}
