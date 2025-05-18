2869cfafc32c8cc86bb7332f7bc23e3564924f50
rndud008 <rndud008@gmail.com>
2025-05-18 14:22:32 +0900

    [자바 ORM 표준 JPA 프로그래밍 - 기본편]- 값 타입의 비교

--- src/main/java/hello/hellobasic/Address.java
+++ src/main/java/hello/hellobasic/Address.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Embeddable;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Embeddable
 @Getter
 public class Address {
@@ -21,4 +23,16 @@ public class Address {
         this.zipcode = zipcode;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(city, address.city) && Objects.equals(street, address.street) && Objects.equals(zipcode, address.zipcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, zipcode);
+    }
 }
--- src/main/java/hello/hellobasic/ValueMain.java
+++ src/main/java/hello/hellobasic/ValueMain.java
@@ -23,5 +23,14 @@ public class ValueMain {
         // 참조값 공유
 //        변경 X
 
+        int a = 10;
+        int b = 10;
+        System.out.println("a == b: " + (a==b));
+
+        Address address1 = new Address("city","street","10000");
+        Address address2 = new Address("city","street","10000");
+        System.out.println("(address1 == address2): " + (address1 == address2));
+        System.out.println("(address1.equals(address2)): " + (address1.equals(address2)));
+
     }
 }
