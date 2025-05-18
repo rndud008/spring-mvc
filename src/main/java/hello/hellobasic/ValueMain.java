e853eb922d1f8ab45206d5aaa31f4b0171c39731
rndud008 <rndud008@gmail.com>
2025-05-18 13:47:10 +0900

    [자바 ORM 표준 JPA 프로그래밍 - 기본편]- 값 타입

---
+++ src/main/java/hello/hellobasic/ValueMain.java
@@ -0,0 +1,27 @@
+package hello.hellobasic;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+
+public class ValueMain {
+    public static void main(String[] args) {
+//        int a = 10;
+//        int b = a;
+//
+//        b =20;
+//
+//        System.out.println("a = " + a);
+//        System.out.println("b = " + b);
+        // 기본타입은 항상 값을 복사
+//         공유 X
+
+
+//        Integer a = 10;
+//        Integer b = a;
+//
+//        System.out.println("a = " + a);
+//        System.out.println("b = " + b);
+        // 참조값 공유
+//        변경 X
+
+    }
+}
